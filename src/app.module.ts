11c20e00e35a10dae1cf2bb51117246deb533063
Douglas Batista <douglas.silva@zooxsmart.com>
2024-08-26 21:30:15 -0300

    chore: adiciona mongoose ao projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TutorialModule } from './tutorial/tutorial.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, TutorialModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    TutorialModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
